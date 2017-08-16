cc5682a319d7c381beea6e5283ae1792fa0c7866
Anna Ho <annayqho@gmail.com>
2017-08-16 15:35:23 -0700

    input points

--- gen_otfm_grid.py
+++ gen_otfm_grid.py
@@ -12,11 +12,10 @@ def get_coords():
     return c1,c2,c3,c4
 
 
-def plot_rec():
-    plt.scatter(c1.ra, c1.dec, c='blue', label="C1", lw=0, s=50)
-    plt.scatter(c2.ra, c2.dec, c='magenta', label="C2", lw=0, s=50)
-    plt.scatter(c3.ra, c3.dec, c='green', label="C3", lw=0, s=50)
-    plt.scatter(c4.ra, c4.dec, c='black', label="C4", lw=0, s=50)
+def plot_rec(cs):
+    colors = ['red', 'orange', 'yellow', 'green', 'cyan', 'blue', 'purple', 'black']
+    for ii,c in enumerate(cs):
+        plt.scatter(c.ra, c.dec, c=colors[ii], label=ii, lw=0, s=50)
     plt.legend()
 
 
@@ -74,43 +73,48 @@ def plot_cal():
 
 
 def run():
-    c1 = SkyCoord(11.659, 25.445, frame='icrs', unit='deg')
-    c2 = SkyCoord(11.527, 24.975, frame='icrs', unit='deg') 
-    c3 = SkyCoord(13.981, 24.140, frame='icrs', unit='deg')
-    c4 = SkyCoord(14.121, 24.608, frame='icrs', unit='deg') 
-    #plot_rec()
+    c1 = SkyCoord(157.035, -29.211, frame='icrs', unit='deg')
+    c2 = SkyCoord(156.551, -28.839, frame='icrs', unit='deg') 
+    c3 = SkyCoord(157.863, -26.870, frame='icrs', unit='deg')
+    c4 = SkyCoord(158.343, -27.238, frame='icrs', unit='deg')
+    c5 = SkyCoord(157.476, -29.111, frame='icrs', unit='deg')
+    c6 = SkyCoord(157.014, -28.758, frame='icrs', unit='deg')
+    c7 = SkyCoord(158.516, -26.503, frame='icrs', unit='deg')
+    c8 = SkyCoord(158.974, -26.850, frame='icrs', unit='deg')
+    cs = [c1, c2, c3, c4, c5, c6, c7, c8]
+    plot_rec(cs)
 
-    gen_left()
-    gen_right()
-
-    plt.xlabel("RA (deg)")
-    plt.ylabel("Dec (deg)")
-
-    ras = np.sort(np.array([c1.ra.value, c2.ra.value, c3.ra.value, c4.ra.value]))
-    decs = np.sort(np.array([c1.dec.value, c2.dec.value, c3.dec.value, c4.dec.value]))
-
-    dec_grid = np.arange(decs[2], decs[3]+0.1, 0.125)
-    x = np.repeat(min(ras),len(dec_grid))
-    plt.scatter(x, dec_grid, color='k', s=1)
-    coord_grid = SkyCoord(x, dec_grid, unit='deg')
-    for coord in coord_grid:
-        print(coord.ra.hms)
-        print(coord.dec.dms)
-
-    dec_grid = np.arange(decs[0], decs[1]+0.1, 0.125)
-    x = np.repeat(max(ras),len(dec_grid))
-    plt.scatter(x, dec_grid, color='k', s=1)
-    coord_grid = SkyCoord(x, dec_grid, unit='deg')
-    for coord in coord_grid:
-        print(coord.ra.hms)
-        print(coord.dec.dms)
-
-
-    plot_fluxcal()
-    plot_cal()
+#     gen_left()
+#     gen_right()
+# 
+#     plt.xlabel("RA (deg)")
+#     plt.ylabel("Dec (deg)")
+# 
+#     ras = np.sort(np.array([c1.ra.value, c2.ra.value, c3.ra.value, c4.ra.value]))
+#     decs = np.sort(np.array([c1.dec.value, c2.dec.value, c3.dec.value, c4.dec.value]))
+# 
+#     dec_grid = np.arange(decs[2], decs[3]+0.1, 0.125)
+#     x = np.repeat(min(ras),len(dec_grid))
+#     plt.scatter(x, dec_grid, color='k', s=1)
+#     coord_grid = SkyCoord(x, dec_grid, unit='deg')
+#     for coord in coord_grid:
+#         print(coord.ra.hms)
+#         print(coord.dec.dms)
+# 
+#     dec_grid = np.arange(decs[0], decs[1]+0.1, 0.125)
+#     x = np.repeat(max(ras),len(dec_grid))
+#     plt.scatter(x, dec_grid, color='k', s=1)
+#     coord_grid = SkyCoord(x, dec_grid, unit='deg')
+#     for coord in coord_grid:
+#         print(coord.ra.hms)
+#         print(coord.dec.dms)
+# 
+# 
+#     plot_fluxcal()
+#     plot_cal()
     plt.show()
-    plt.legend(loc="lower right")
-    plt.ylim(24,34)
-
+#     plt.legend(loc="lower right")
+#     plt.ylim(24,34)
+# 
 if __name__=="__main__":
     run()
